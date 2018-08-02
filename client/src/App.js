c4b5cda388a8f8853093f188e4eb0840deefadaa
clowery <clowery52916@gmail.com>
2018-08-02 11:55:56 -0400

    got single page app and nav bar done, basic styling, nothing fancy at all yet

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,41 @@
-import React, { Component } from 'react';
-import { Route, Link } from "react-router-dom";
+import React, {Component} from 'react';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Photos from './components/photos/Photos'
+import Footer from "./components/footer/Footer";
 
 export default class App extends Component {
-render() {
-return(
-<div>Test</div>
-)}}
+  render() {
+    return (
+      <HashRouter>
+      <div>
+      <h1>Ingraham's Woodworking Shop</h1>
+      <ul className="header">
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">Info</NavLink>
+        </li>
+        <li>
+          <NavLink to="/photos">Photos</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+      <div className="content">
+      <Route exact path='/' component={Home}/>
+      <Route path='/about' component={About}/>
+      <Route path='/photos' component={Photos}/>
+      <Route path='/contact' component={Footer}/>
+      </div>
+    </div>
+  </HashRouter>)
+  }
+}
