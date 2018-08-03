e556e8d178e6c4c36eba0cff2bff27fb260acd04
clowery <clowery52916@gmail.com>
2018-08-02 20:17:25 -0400

    got nav responsive, still want to change it to a button or icon and adjust the show portion on the nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,43 @@
 import React, {Component} from 'react';
-import {
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
+import {Route, NavLink, HashRouter} from "react-router-dom";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Photos from './components/photos/Photos'
 import Footer from "./components/footer/Footer";
+import styled from 'styled-components';
 
 export default class App extends Component {
   render() {
-    return (
-      <HashRouter>
+    return (<HashRouter>
+
       <div>
-      <h1>Ingraham's Woodworking Shop</h1>
-      <ul className="header">
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">Info</NavLink>
-        </li>
-        <li>
-          <NavLink to="/photos">Photos</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-      </ul>
-      <div className="content">
-      <Route exact path='/' component={Home}/>
-      <Route path='/about' component={About}/>
-      <Route path='/photos' component={Photos}/>
-      <Route path='/contact' component={Footer}/>
+
+        <h1>Ingraham's Woodworking Shop</h1>
+        <label htmlFor="show-menu" className="show-menu">Show Menu</label>
+        <input type="checkbox" id="show-menu" role="button"/>
+        <ul className="navbar" id="menu">
+          <li>
+            <NavLink className='link' to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink className='link' to="/about">Info</NavLink>
+          </li>
+          <li>
+            <NavLink className='link' to="/photos">Photos</NavLink>
+          </li>
+          <li>
+            <NavLink className='link' to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+
+        <div className="content">
+          <Route exact="exact" path='/' component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route path='/photos' component={Photos}/>
+          <Route path='/contact' component={Footer}/>
+        </div>
+
       </div>
-    </div>
-  </HashRouter>)
+    </HashRouter>)
   }
 }
