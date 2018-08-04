690cedadce017b77523650363a59e62afdf5e9c1
bethschoenfeld <bethingraham3@gmail.com>
2018-08-04 14:10:51 -0400

    Resolving merge conflicts

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import App from '../App.js';
-import { BrowserRouter as Router } from "react-router-dom";
-=======
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/styles.css'
->>>>>>> 842d053509665f9b106bcef8e960f08216d8e6c5
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
