13350f76b6b34acb7a1cb2d1c5dfa04938232f66
bethschoenfeld <bethingraham3@gmail.com>
2018-08-04 14:09:28 -0400

    Switching to Bob Ross Lipsum to further differentiate each view.

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 
 export default class About extends Component {
-render() {
-return(
-  <div>
-    <h2>About</h2>
-    <p>Cras facilisis urna ornare ex volutpat, et
-    convallis erat elementum. Ut aliquam, ipsum vitae
-    gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-    metus nec massa. Maecenas hendrerit laoreet augue
-    nec molestie. Cum sociis natoque penatibus et magnis
-    dis parturient montes, nascetur ridiculus mus.</p>
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
 
-    <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
-  </div>
-)}}
+        <p>This is the time to get out all your flustrations, much better than kicking the dog around the house or taking it out on your spouse. You can create beautiful things - but you have to see them in your mind first. Isn't that fantastic? You can just push a little tree out of your brush like that. A little happy sunlight shining through there.</p>
+
+        <p>You've got to learn to fight the temptation to resist these things. Just let them happen. We artists are a different breed of people. We're a happy bunch. Every time you practice, you learn more. We don't need any guidelines or formats. All we need to do is just let it flow right out of us. Get away from those little Christmas tree things we used to make in school.</p>
+
+        <p>If you do too much it's going to lose its effectiveness. That's crazy. Use what you see, don't plan it.</p>
+
+        <p>It's a very cold picture, I may have to go get my coat. It’s about to freeze me to death. This is truly an almighty mountain. We'll put some happy little leaves here and there. All you have to learn here is how to have fun.</p>
+
+        <p>Now it's beginning to make a little sense. Imagination is the key to painting. If you overwork it you become a cloud killer. There's nothing worse than a cloud killer. Little trees and bushes grow however makes them happy.</p>
+
+        <p>Now we can begin working on lots of happy little things. If you don't like it - change it. It's your world. There's nothing wrong with having a tree as a friend.</p>
+
+        <p>We have a fantastic little sky! And I know you're saying, 'Oh Bob, you've done it this time.' And you may be right. Only eight colors that you need. If you hypnotize it, it will go away. What the devil. Trees cover up a multitude of sins.</p>
+
+
+      </div>
+    )
+  }
+}
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -5,14 +5,21 @@ class Home extends Component {
     return (
       <div>
         <h2>Home</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
+        <p>You better get your coat out, this is going to be a cold painting. Nothing wrong with washing your brush. Very easy to work these to death. Painting should do one thing. It should put happiness in your heart. Trees grow in all kinds of ways. They're not all perfectly straight. Not every limb is perfect.</p>
 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+        <p>Isn't it great to do something you can't fail at? Get tough with it, get strong. We don't have to be committed. We are just playing here. We start with a vision in our heart, and we put it on canvas. If you don't like it - change it. It's your world.</p>
+
+        <p>We don't have anything but happy trees here. We're not trying to teach you a thing to copy. We're just here to teach you a technique, then let you loose into the world. Put it in, leave it alone. At home you have unlimited time. The very fact that you're aware of suffering is enough reason to be overjoyed that you're alive and can experience it.</p>
+
+        <p>Paint anything you want on the canvas. Create your own world. Life is too short to be alone, too precious. Share it with a friend. I guess that would be considered a UFO. A big cotton ball in the sky. You can't make a mistake. Anything that happens you can learn to use - and make something beautiful out of it.</p>
+
+        <p>With practice comes confidence. Every day I learn. Everyone wants to enjoy the good parts - but you have to build the framework first.</p>
+
+        <p>If these lines aren't straight, your water's going to run right out of your painting and get your floor wet. This is unplanned it really just happens. You can spend all day playing with mountains. This is gonna be a happy little seascape. This is your creation - and it's just as unique and special as you are. We'll put some happy little leaves here and there.</p>
+
+        <p>Just take out whatever you don't want. It'll change your entire perspective. The only thing worse than yellow snow is green snow. You create the dream - then you bring it into your world. That's why I paint - because I can create the kind of world I want - and I can make this world as happy as I want it. Maybe there was an old trapper that lived out here and maybe one day he went to check his beaver traps, and maybe he fell into the river and drowned. Let your heart take you to wherever you want to be.</p>
+
+
       </div>
     );
   }
